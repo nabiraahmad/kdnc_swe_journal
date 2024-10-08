963ad741cf41803255faff8a1e13cc36f80e8ec8
Nabira <nabiraahmad03@gmail.com>
2024-10-08 16:13:20 -0400

    started people.py & created get_people

--- people.py
+++ people.py
@@ -4,6 +4,20 @@ This module interfaces to our user data.
 
 MIN_USER_NAME_LEN = 2
 
+# people fields:
+NAME = 'name'
+ROLES = 'roles'
+AFFILIATION  = 'affiliation'
+EMAIL = 'email'
+
+
+
+"""
+PARAMETERS: None
+RETURNS: Dictionary of users keyed on user email
+NOTE: Each user email is a key for another dictionary
+"""
+
 def get_people():
-	"""
-	
+	users = {'na2819@nyu.edu': {}}
+	return users	
