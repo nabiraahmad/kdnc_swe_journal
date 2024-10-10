05c370fa094e1c529a61e5019ff436bf4a610c08
Dariana Gonzalez <100489203+dglezz@users.noreply.github.com>
2024-10-10 00:36:54 -0400

    fixed indentation issues in people.py.. added delete_person to file

--- data/people.py
+++ data/people.py
@@ -7,25 +7,25 @@ MIN_USER_NAME_LEN = 2
 # people fields:
 NAME = 'name'
 ROLES = 'roles'
-AFFILIATION  = 'affiliation'
+AFFILIATION = 'affiliation'
 EMAIL = 'email'
 
 TEST_EMAIL = 'na2819@nyu.edu'
 DEL_EMAIL = 'dg3729@nyu.edu'
 
 TEST_PERSON_DICT = {
-	TEST_EMAIL: {
-		NAME: 'Nabira Ahmad',
-		ROLES: [],
-		AFFILIATION: 'NYU',
-		EMAIL: TEST_EMAIL,
-	},
-	DEL_EMAIL: {
-		NAME: 'Dariana Gonzalez',
-		ROLES: [],
-		AFFILIATION: 'NYU',
-		EMAIL: DEL_EMAIL,
-	},
+    TEST_EMAIL: {
+        NAME: 'Nabira Ahmad',
+        ROLES: [],
+        AFFILIATION: 'NYU',
+        EMAIL: TEST_EMAIL,
+    },
+    DEL_EMAIL: {
+        NAME: 'Dariana Gonzalez',
+        ROLES: [],
+        AFFILIATION: 'NYU',
+        EMAIL: DEL_EMAIL,
+    },
 }
 
 """
@@ -34,15 +34,16 @@ RETURNS: Dictionary of users keyed on user email
 NOTE: Each user email is a key for another dictionary
 """
 
+
 def get_people():
-	people = TEST_PERSON_DICT
-	return people
+    people = TEST_PERSON_DICT
+    return people
 
 
 def delete_person(_id):
-	people = get_people()
-	if _id in people:
-		del people[_id]
-		return _id
-	else:
-		return None
+    people = get_people()
+    if _id in people:
+        del people[_id]
+        return _id
+    else:
+        return None
