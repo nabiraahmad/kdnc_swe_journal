02ea3e4551d8397dc7dcc5a5ce228e189eb2fd82
Ken <ktn3138@nyu.edu>
2024-10-16 09:44:07 -0400

    created text.py and implemented read() function

---
+++ data/text.py
@@ -0,0 +1,31 @@
+"""
+This module interfaces to our user data.
+"""
+
+# fields
+KEY = 'key'
+TITLE = 'title'
+TEXT = 'text'
+EMAIL = 'email'
+
+TEST_KEY = 'HomePage'
+DEL_KEY = 'DeletePage'
+
+text_dict = {
+    TEST_KEY: {
+        KEY: TEST_KEY,
+        TITLE: 'Home Page',
+        TEXT: 'This is a journal about health and fitness.',
+        EMAIL: 'contact@healthjournal.com',
+    },
+    DEL_KEY: {
+        KEY: DEL_KEY,
+        TITLE: 'Delete Page',
+        TEXT: 'This is a journal about health and fitness.',
+        EMAIL: 'admin@healthjournal.com',
+    },
+}
+
+def read():
+    text = text_dict
+    return text
