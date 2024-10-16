c14827a26cf918e455b39c86a6c8b3083b9a00c4
Nabira <nabiraahmad03@gmail.com>
2024-10-16 18:17:59 -0400

    added test_people.py and created get, delete, and create

---
+++ data/tests/test_people.py
@@ -0,0 +1,33 @@
+import pytest
+
+import data.people as ppl
+
+
+def test_get_people():
+    people = ppl.get_people()
+    assert isinstance(people, dict)
+    assert len(people) > 0
+    # check for string IDs:
+    for _id, person in people.items():
+        assert isinstance(_id, str)
+        assert ppl.NAME in person
+
+
+def test_delete():
+    people = ppl.get_people()
+    old_len = len(people)
+    ppl.delete_person(ppl.DEL_EMAIL)
+    people = ppl.get_people()
+    assert len(people) < old_len
+    assert ppl.DEL_EMAIL not in people
+
+
+ADD_EMAIL = 'bill@nyu.edu'
+
+
+def test_create_person():
+    people = ppl.get_people()
+    assert ADD_EMAIL not in people
+    ppl.create_Person('Nabira Ahmad', 'NYU', ADD_EMAIL)
+    people = ppl.get_people()
+    assert ADD_EMAIL in people
