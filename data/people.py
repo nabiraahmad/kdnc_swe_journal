3aaf441c1cafa8c1b8d5a75771c1f0ac5b4bd793
Dariana Gonzalez <100489203+dglezz@users.noreply.github.com>
2024-10-24 19:05:06 -0400

    added the regular expression and added functions to check validity of emails to people.py

--- data/people.py
+++ data/people.py
@@ -1,6 +1,7 @@
 """
 This module interfaces to our user data.
 """
+import re
 
 MIN_USER_NAME_LEN = 2
 
@@ -34,6 +35,12 @@ RETURNS: Dictionary of users keyed on user email
 NOTE: Each user email is a key for another dictionary
 """
 
+CHAR_OR_DIGIT = r"^(?![a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$).*"
+
+
+def is_valid_email(email: str) -> bool:
+    return re.match(f"{CHAR_OR_DIGIT}.*@{CHAR_OR_DIGIT}.*", email)
+
 
 def get_people():
     people = TEST_PERSON_DICT
@@ -59,6 +66,15 @@ def update_person(name: str, affiliation: str, email: str):
     else:
         # If the person does not exist, raise an error
         raise ValueError(f'Person with email {email} does not exist')
+
+
+def is_valid_person(name: str, affiliation: str, email: str) -> bool:
+    people = get_people()
+    if email in people:
+        raise ValueError(f'Adding duplicate {email=}')
+    if not is_valid_email(email):
+        raise ValueError(f'Invalid email: {email}')
+    return True
 
 
 def create_person(name: str, affiliation: str, email: str):
