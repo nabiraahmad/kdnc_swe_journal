44c4fca5f07a83e713693267b87e3ff49b3d55ec
Dariana Gonzalez <100489203+dglezz@users.noreply.github.com>
2024-11-21 22:52:36 -0500

    updated read and get_one in db_connect

--- data/db_connect.py
+++ data/db_connect.py
@@ -38,6 +38,11 @@ def connect_db():
     return client
 
 
+def convert_mongo_id(doc: dict):
+     if MONGO_ID in doc:
+         doc[MONGO_ID] = str(doc[MONGO_ID])
+
+
 def create(collection, doc, db=SE_DB):
     """
     Insert a single doc into collection.
@@ -46,15 +51,13 @@ def create(collection, doc, db=SE_DB):
     return client[db][collection].insert_one(doc)
 
 
-def fetch_one(collection, filt, db=SE_DB):
+def get_one(collection, filt, db=SE_DB):
     """
     Find with a filter and return on the first doc found.
     Return None if not found.
     """
     for doc in client[db][collection].find(filt):
-        if MONGO_ID in doc:
-            # Convert mongo ID to a string so it works as JSON
-            doc[MONGO_ID] = str(doc[MONGO_ID])
+        convert_mongo_id(doc)
         return doc
 
 
@@ -62,7 +65,8 @@ def del_one(collection, filt, db=SE_DB):
     """
     Find with a filter and return on the first doc found.
     """
-    client[db][collection].delete_one(filt)
+    del_result = client[db][collection].delete_one(filt)
+    return del_result.deleted_count
 
 
 def update_doc(collection, filters, update_dict, db=SE_DB):
@@ -77,6 +81,8 @@ def read(collection, db=SE_DB, no_id=True) -> list:
     for doc in client[db][collection].find():
         if no_id:
             del doc[MONGO_ID]
+        else: 
+            convert_mongo_id(doc)
         ret.append(doc)
     return ret
 
