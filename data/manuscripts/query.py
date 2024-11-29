42fe3eb0c638cf1edd50b93b85d45b59a27665b0
Nabira <nabiraahmad03@gmail.com>
2024-11-28 23:40:56 -0500

    added query.py

---
+++ data/manuscripts/query.py
@@ -0,0 +1,41 @@
+# type of states:
+COPY_EDIT = 'CED'
+IN_REF_REV = 'REV'
+REJECTED = 'REJ'
+SUBMITTED = 'SUB'
+TEST_STATE = SUBMITTED
+VALID_STATES = [
+    COPY_EDIT,
+    IN_REF_REV,
+    REJECTED,
+    SUBMITTED,
+]
+def get_states() -> list:
+    return VALID_STATES
+def is_valid_state(state: str) -> bool:
+    return state in VALID_STATES
+
+
+# type of actions:
+ACCEPT = 'ACC'
+ASSIGN_REF = 'ARF'
+DONE = 'DON'
+REJECT = 'REJ'
+
+
+# testing purposes:
+TEST_ACTION = ACCEPT
+VALID_ACTIONS = [
+    ACCEPT,
+    ASSIGN_REF,
+    DONE,
+    REJECT,
+]
+
+
+def get_actions() -> list:
+    return VALID_ACTIONS
+
+
+def is_valid_action(action: str) -> bool:
+    return action in VALID_ACTIONS
