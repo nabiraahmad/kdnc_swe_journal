359030f59682d1c64c3632ae554a8294c8e45532
Dariana Gonzalez <100489203+dglezz@users.noreply.github.com>
2024-12-12 02:37:14 -0500

    added new print statements in query.py

--- data/manuscripts/query.py
+++ data/manuscripts/query.py
@@ -134,5 +134,13 @@ def handle_action(curr_state, action, **kwargs) -> str:
 
 
 def main():
-    print(handle_action(SUBMITTED, ASSIGN_REF, SAMPLE_MANU))
-    print(handle_action(SUBMITTED, REJECT, SAMPLE_MANU))
+    print(handle_action(SUBMITTED, ASSIGN_REF,
+                        manu=SAMPLE_MANU, ref='Jack'))
+    print(handle_action(IN_REF_REV, ASSIGN_REF, manu=SAMPLE_MANU,
+                        ref='Jill', extra='Extra!'))
+    print(handle_action(IN_REF_REV, DELETE_REF, manu=SAMPLE_MANU,
+                        ref='Jill'))
+    print(handle_action(IN_REF_REV, DELETE_REF, manu=SAMPLE_MANU,
+                        ref='Jack'))
+    print(handle_action(SUBMITTED, WITHDRAW, manu=SAMPLE_MANU))
+    print(handle_action(SUBMITTED, REJECT, manu=SAMPLE_MANU))
