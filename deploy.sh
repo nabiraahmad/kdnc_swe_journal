c33b50b731b51d13c66219e5f7c2cdf70412e5d4
Ken <ktn3138@nyu.edu>
2024-12-16 18:15:18 -0500

    set up github actions for pythonanywhere

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # This shell script deploys a new version to a server.
 
-PROJ_DIR=demo-repo4
-VENV=Fall2023
-PA_DOMAIN="Fall2023.pythonanywhere.com"
-PA_USER='Fall2023'
+PROJ_DIR=kdnc_swe_journal
+VENV=venv
+PA_DOMAIN="ktn3138.pythonanywhere.com"
+PA_USER='ktn3138'
 echo "Project dir = $PROJ_DIR"
 echo "PA domain = $PA_DOMAIN"
 echo "Virtual env = $VENV"
