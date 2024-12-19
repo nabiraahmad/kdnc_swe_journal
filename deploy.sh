71584d1ce460ecee72963fd830b295f025408f4a
Ken <ktn3138@nyu.edu>
2024-12-18 19:55:38 -0500

    debugging deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 # This shell script deploys a new version to a server.
 
-PROJ_DIR=kdnc_swe_journal
-VENV=venv
+PROJ_DIR="kdnc_swe_journal"
+VENV="venv"
 PA_DOMAIN="ktn3138.pythonanywhere.com"
-PA_USER='ktn3138'
+PA_USER="ktn3138"
+
 echo "Project dir = $PROJ_DIR"
 echo "PA domain = $PA_DOMAIN"
 echo "Virtual env = $VENV"
+echo "PA user = $PA_USER"
 
-if [ -z "$DEMO_PA_PWD" ]
-then
+if [ -z "$DEMO_PA_PWD" ]; then
     echo "The PythonAnywhere password var (DEMO_PA_PWD) must be set in the env."
     exit 1
 fi
 
-echo "PA user = $PA_USER"
 echo "PA password = $DEMO_PA_PWD"
 
 echo "SSHing to PythonAnywhere."
 sshpass -p $DEMO_PA_PWD ssh -o "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
     cd /home/$PA_USER/$PROJ_DIR
-    PA_USER=$PA_USER PROJ_DIR=~/$PROJ_DIR VENV=$VENV PA_DOMAIN=$PA_DOMAIN ./rebuild.sh
+    ./rebuild.sh
 EOF
